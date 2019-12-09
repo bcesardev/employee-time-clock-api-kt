12b4d4b7c3339f0ef8c6acd5b08121b3462672bd
Bruno Cesar <bcesar.bruno@gmail.com>
2019-12-09 17:31:47 -0300

    First Commit

---
+++ src/main/kotlin/com/bcesardev/employeetimeclockapikt/EmployeeTimeClockApiKtApplication.kt
@@ -0,0 +1,11 @@
+package com.bcesardev.employeetimeclockapikt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeTimeClockApiKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmployeeTimeClockApiKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/bcesardev/employeetimeclockapikt/EmployeeTimeClockApiKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bcesardev.employeetimeclockapikt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeeTimeClockApiKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
